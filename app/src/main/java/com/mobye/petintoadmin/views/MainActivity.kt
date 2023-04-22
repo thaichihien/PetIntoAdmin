5a4c205588478c1abe3e60131af9a52e7e7731ca
thaichihien <79949235+thaichihien@users.noreply.github.com>
2023-04-22 08:25:28 +0700

    Initial commit

---
+++ app/src/main/java/com/mobye/petintoadmin/views/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.mobye.petintoadmin.views
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.mobye.petintoadmin.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
